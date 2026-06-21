1e0df164c120e9042cc449788a4ed8617a93947a
vinod8990 <iamvinodkumarhtm@gmail.com>
2026-06-21 18:15:10 +0530

    Landscape layout adjusted

--- scripts/splash_scene.gd
+++ scripts/splash_scene.gd
@@ -9,6 +9,24 @@ const BOARD_SCENE := "res://scenes/board_scene.tscn"
 func _ready() -> void:
 	challenge_container_landscape.play_pressed.connect(_on_play_pressed)
 	challenge_container_portrait.play_pressed.connect(_on_play_pressed)
+	%OrientationListenerToggler.on_size_changed.connect(_on_size_changed)
+
+
+func _exit_tree() -> void:
+	%OrientationListenerToggler.on_size_changed.disconnect(_on_size_changed)
+
+
+func _on_size_changed(_is_portrait: bool) -> void:
+	var l = %LandscapeContent
+	l.custom_minimum_size = Vector2.ZERO
+	l.anchor_left = 0
+	l.anchor_right = 1.0
+	l.anchor_top = 0
+	l.anchor_bottom = 1.0
+	l.offset_left = 0
+	l.offset_right = 0
+	l.offset_top = 0
+	l.offset_bottom = 0
 
 
 func _on_play_pressed() -> void:
