812894de9b968f47458b853f7b541316c63ac5bf
vinod8990 <iamvinodkumarhtm@gmail.com>
2026-09-22 23:01:41 +0530

    Updated the challenge title according to the date selected

--- prefabs/splash_challenge_container/splash_challenge_container_component.gd
+++ prefabs/splash_challenge_container/splash_challenge_container_component.gd
@@ -1,6 +1,11 @@
 class_name SplashChallengeContainerComponent
 extends BaseComponent
 
+const SHORT_MONTH_NAMES: Array[String] = [
+	"JAN", "FEB", "MAR", "APR", "MAY", "JUNE",
+	"JULY", "AUG", "SEPT", "OCT", "NOV", "DEC",
+]
+
 var _launcher: SplashChallengeLauncherComponent
 
 
@@ -13,6 +18,7 @@ func _ready() -> void:
 
 
 func _update_date(date_key: String) -> void:
+	get_node("../../Background/Content/TitleContainer/Title").text = _get_challenge_title(date_key)
 	var session := GameDB.create_challenge_session(date_key)
 	if session == null:
 		return
@@ -22,6 +28,13 @@ func _update_date(date_key: String) -> void:
 		var seconds := int(level.time_limit_seconds)
 		var minutes := int(ceil(float(seconds) / 60.0))
 		get_node("../../Background/Content/%s/Details/Time" % str(level.difficulty).capitalize()).text = "Complete in %d min." % minutes
+
+
+func _get_challenge_title(date_key: String) -> String:
+	if date_key == DailyChallengeService.get_today_key():
+		return "TODAY'S CHALLENGE"
+	var date := Time.get_date_dict_from_unix_time(DailyChallengeService._date_key_to_unix(date_key))
+	return "%s %d - CHALLENGE" % [SHORT_MONTH_NAMES[date.month - 1], int(date.day)]
 
 
 func _play(difficulty: String) -> void:
